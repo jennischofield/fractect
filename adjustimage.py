e0fd7c92c4b60e647ba4c1afbde33cf8c24acbbf
Jenni Schofield <jennischofield02@icloud.com>
2024-04-02 22:13:39 +0100

    Turned on linter properly and added in code for the train/test for resnext

--- adjustimage.py
+++ adjustimage.py
@@ -1,24 +1,28 @@
 # This class was adapted from Martin Evans's (https://stackoverflow.com/users/4985733/martin-evans)
 # Stack Overflow answer to Max Gordon's question (https://stackoverflow.com/users/409508/max-gordon)
-# The original answer can be found here: https://stackoverflow.com/questions/32435488/align-x-ray-images-find-rotation-rotate-and-crop 
+# The original answer can be found here: https://stackoverflow.com/questions/32435488/align-x-ray-images-find-rotation-rotate-and-crop
 import cv2
 import numpy as np
 from PIL import Image
 import math
+
+
 class AdjustImage(object):
     THRESHOLD = 240
+
     def subimage(self, image, center, theta, width, height):
         if 45 < theta <= 90:
             theta = theta - 90
             width, height = height, width
 
-        theta *= math.pi / 180 # convert to rad
+        theta *= math.pi / 180  # convert to rad
         v_x = (math.cos(theta), math.sin(theta))
         v_y = (-math.sin(theta), math.cos(theta))
         s_x = center[0] - v_x[0] * (width / 2) - v_y[0] * (height / 2)
         s_y = center[1] - v_x[1] * (width / 2) - v_y[1] * (height / 2)
-        mapping = np.array([[v_x[0],v_y[0], s_x], [v_x[1],v_y[1], s_y]])
+        mapping = np.array([[v_x[0], v_y[0], s_x], [v_x[1], v_y[1], s_y]])
         return cv2.warpAffine(image, mapping, (width, height), flags=cv2.WARP_INVERSE_MAP, borderMode=cv2.BORDER_REPLICATE)
+
     def __call__(self, image_source):
 
         # Save the original image for error handling later
@@ -29,26 +33,29 @@ class AdjustImage(object):
         dimensions = image_source.ndim
         h, w = image_source.shape[:2]
 
-        #crop out and add back border for additional definition of x-ray vs image
-        image_source = image_source[init_crop:init_crop+(h-init_crop*2), init_crop:init_crop+(w-init_crop*2)]
+        # crop out and add back border for additional definition of x-ray vs image
+        image_source = image_source[init_crop:init_crop +
+                                    (h-init_crop*2), init_crop:init_crop+(w-init_crop*2)]
         # Add back a white border
-        image_source = cv2.copyMakeBorder(image_source, 5,5,5,5, cv2.BORDER_CONSTANT, value=(255,255,255))
+        image_source = cv2.copyMakeBorder(
+            image_source, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=(255, 255, 255))
 
         # Check if image is already in greyscale or not, if not convert it
         if dimensions == 2:
             image_grey = image_source.copy()
         else:
             image_grey = cv2.cvtColor(image_source, cv2.COLOR_BGR2GRAY)
-        #Create the thresh of the the greyscale image - using the threshold of TOZERO_INV (a gradient that cuts to 0)
-        _, image_thresh = cv2.threshold(image_grey, self.THRESHOLD, 255, cv2.THRESH_TOZERO_INV)
+        # Create the thresh of the the greyscale image - using the threshold of TOZERO_INV (a gradient that cuts to 0)
+        _, image_thresh = cv2.threshold(
+            image_grey, self.THRESHOLD, 255, cv2.THRESH_TOZERO_INV)
         image_thresh2 = image_thresh.copy()
 
-        #Get the canny edge detection
+        # Get the canny edge detection
         image_thresh2 = cv2.Canny(image_thresh2, 100, 100, apertureSize=3)
 
         # Get the points from the canny edge detection - sometimes this may error
-        # Sometimes the shape found isn't a rectangle, as the corners of the x-ray are obscured/cut off from 
-        # augmentation. In that case, the original image is returned. The neural network can handle rotation/shear 
+        # Sometimes the shape found isn't a rectangle, as the corners of the x-ray are obscured/cut off from
+        # augmentation. In that case, the original image is returned. The neural network can handle rotation/shear
         # if it has to
         points = cv2.findNonZero(image_thresh2)
 
@@ -63,19 +70,21 @@ class AdjustImage(object):
         box = np.intp(box)
 
         temp = image_source.copy()
-        cv2.drawContours(temp, [box], 0, (255,0,0), 2)
+        cv2.drawContours(temp, [box], 0, (255, 0, 0), 2)
 
         M = cv2.moments(box)
         cx = int(M['m10']/M['m00'])
         cy = int(M['m01']/M['m00'])
 
         # It assumes the image is already incorrectly rotated - so angle of image + 90 to fix it.
-        image_patch = self.subimage(image_source, (cx, cy), (theta+90), height, width)
+        image_patch = self.subimage(
+            image_source, (cx, cy), (theta+90), height, width)
 
         # add back a small border
-        image_patch = cv2.copyMakeBorder(image_patch, 1,1,1,1, cv2.BORDER_CONSTANT, value=(255,255,255))
+        image_patch = cv2.copyMakeBorder(
+            image_patch, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value=(255, 255, 255))
 
-        #Now find the thresh of the original sub image + do edge detection
+        # Now find the thresh of the original sub image + do edge detection
         _, image_thresh = cv2.threshold(image_patch, self.THRESHOLD, 255, 1)
         image_thresh = cv2.Canny(image_thresh, 100, 100, 3)
         points = cv2.findNonZero(image_thresh)
@@ -92,35 +101,37 @@ class AdjustImage(object):
 
         # Find closest fitting image size and warp/crop to fit
 
-        x,y,w,h = cv2.boundingRect(hull)
-        target_corners = np.array([[0,0],[w,0],[w,h],[0,h]], np.float32)
+        x, y, w, h = cv2.boundingRect(hull)
+        target_corners = np.array([[0, 0], [w, 0], [w, h], [0, h]], np.float32)
 
         # Sort hull into top left,top right,bottom right,bottom left order.
 
-        source_corners = hull.reshape(-1,2).astype('float32')
+        source_corners = hull.reshape(-1, 2).astype('float32')
         min_dist = 100000
         index = 0
 
         for n in range(len(source_corners)):
-            x,y = source_corners[n]
-            dist = math.hypot(x,y)
+            x, y = source_corners[n]
+            dist = math.hypot(x, y)
 
             if dist < min_dist:
                 index = n
                 min_dist = dist
 
         # Rotate the array so top left is first
-                
-        source_corners = np.roll(source_corners , -(2*index))
+
+        source_corners = np.roll(source_corners, -(2*index))
 
         try:
             # return the adjusted image
-            transform = cv2.getPerspectiveTransform(np.float32(source_corners), np.float32(target_corners))
-            var = Image.fromarray(cv2.warpPerspective(image_patch, transform, (w,h))).convert('RGB')
+            transform = cv2.getPerspectiveTransform(
+                np.float32(source_corners), np.float32(target_corners))
+            var = Image.fromarray(cv2.warpPerspective(
+                image_patch, transform, (w, h))).convert('RGB')
             return var
-        except Exception as inst:
-            # We want to return the original image - occasionally (mainly with augmented images) 
-            # the generated thresh would have more than 4 points. In that case, it's fine for 
+        except:
+            # We want to return the original image - occasionally (mainly with augmented images)
+            # the generated thresh would have more than 4 points. In that case, it's fine for
             # an image not to be adjusted.
             return image_source_original
 
--- checkdatasetbalance.py
+++ checkdatasetbalance.py
@@ -33,14 +33,17 @@ single_counter = Counter(single_fractures)
 multi_counter = Counter(multi_fractures)
 unbroken_counter = Counter(unbroken)
 path = ''
+
+
 def gather_lists(path):
-    for filename in glob.glob(os.path.join(path, '*.json')): #only process .JSON files in folder.      
+    # only process .JSON files in folder.
+    for filename in glob.glob(os.path.join(path, '*.json')):
         with open(filename, encoding='utf-8', mode='r') as currentFile:
-            data=currentFile.read().replace('\n', '')
+            data = currentFile.read().replace('\n', '')
             keyword = json.loads(data)["tags"][0]
             tags = json.loads(data)["tags"]
             if type(keyword) is dict and keyword.get('name') == 'ao_classification':
-                if ';' in keyword.get('value') :
+                if ';' in keyword.get('value'):
                     multi_fractures.append(keyword.get('value'))
                     multi_fracture_files.append(filename)
                     if 'side_right' in tags:
@@ -56,7 +59,8 @@ def gather_lists(path):
                     elif 'projection_oblique' in tags:
                         multi_fracture_oblique.append(filename)
                     else:
-                        print(f"Projection Label Missing {filename} has tag {tags}")
+                        print(
+                            f"Projection Label Missing {filename} has tag {tags}")
                 else:
                     single_fractures.append(keyword.get('value'))
                     single_fracture_files.append(filename)
@@ -73,7 +77,8 @@ def gather_lists(path):
                     elif 'projection_oblique' in tags:
                         single_fracture_oblique.append(filename)
                     else:
-                        print(f"Projection Label Missing {filename} has tag {tags}")
+                        print(
+                            f"Projection Label Missing {filename} has tag {tags}")
             else:
                 unbroken.append(filename)
                 if 'side_right' in tags:
@@ -89,83 +94,106 @@ def gather_lists(path):
                 elif 'projection_oblique' in tags:
                     unbroken_oblique.append(filename)
                 else:
-                    print(f"Projection Label Missing {filename} has tag {tags}")
+                    print(
+                        f"Projection Label Missing {filename} has tag {tags}")
     global single_counter, multi_counter, unbroken_counter
     single_counter = Counter(single_fractures)
     multi_counter = Counter(multi_fractures)
     unbroken_counter = Counter(unbroken)
+
+
 def total_fracture_types_single():
     global list_22, list_23, list_77, list_72, other
     for key, value in single_counter.most_common():
-        if '23'  in key:
+        if '23' in key:
             list_23 += value
-        elif '22'  in key:
+        elif '22' in key:
             list_22 += value
-        elif '77'  in key:
+        elif '77' in key:
             list_77 += value
-        elif '72'  in key:
+        elif '72' in key:
             list_72 += value
         else:
             other += value
-    #print(list_22[0])
+    # print(list_22[0])
+
+
 def show_balances():
-    plt.pie([len(single_fracture_ap),len(single_fracture_lat),len(single_fracture_oblique)], labels = ['Single Fracture Anteroposterior', 'Single Fracture Lateral','Single Fracture Oblique'])
+    plt.pie([len(single_fracture_ap), len(single_fracture_lat), len(single_fracture_oblique)], labels=[
+            'Single Fracture Anteroposterior', 'Single Fracture Lateral', 'Single Fracture Oblique'])
     plt.savefig("data_balance_figures/projection_single_fracture_split")
     plt.clf()
-    plt.pie([len(multi_fracture_ap),len(multi_fracture_lat),len(multi_fracture_oblique)], labels = ['Multi Fracture Anteroposterior', 'Multi Fracture Lateral','Multi Fracture Oblique'])
+    plt.pie([len(multi_fracture_ap), len(multi_fracture_lat), len(multi_fracture_oblique)], labels=[
+            'Multi Fracture Anteroposterior', 'Multi Fracture Lateral', 'Multi Fracture Oblique'])
     plt.savefig("data_balance_figures/projection_multi_fracture_split")
     plt.clf()
-    plt.pie([len(unbroken_ap),len(unbroken_lat),len(unbroken_oblique)], labels = ['Unbroken Anteroposterior', 'Unbroken Lateral','Unbroken Oblique'])
+    plt.pie([len(unbroken_ap), len(unbroken_lat), len(unbroken_oblique)], labels=[
+            'Unbroken Anteroposterior', 'Unbroken Lateral', 'Unbroken Oblique'])
     plt.savefig("data_balance_figures/projection_unbroken_split")
     plt.clf()
-    plt.pie([len(single_fracture_left),len(single_fracture_right)], labels = ['Single Fracture Left', 'Single Fracture Right'])
+    plt.pie([len(single_fracture_left), len(single_fracture_right)],
+            labels=['Single Fracture Left', 'Single Fracture Right'])
     plt.savefig("data_balance_figures/side_single_fracture_split")
     plt.clf()
-    plt.pie([len(multi_fracture_left),len(multi_fracture_right)], labels = ['Multi Fracture Left', 'Multi Fracture Right'])
+    plt.pie([len(multi_fracture_left), len(multi_fracture_right)],
+            labels=['Multi Fracture Left', 'Multi Fracture Right'])
     plt.savefig("data_balance_figures/side_multi_fracture_split")
     plt.clf()
-    plt.pie([len(unbroken_left),len(unbroken_right)], labels = ['Unbroken Left', 'Unbroken Right'])
+    plt.pie([len(unbroken_left), len(unbroken_right)],
+            labels=['Unbroken Left', 'Unbroken Right'])
     plt.savefig("data_balance_figures/side_unbroken_split")
     plt.clf()
-    final_list_single = zip([float(v) for v in single_counter.values()],[k for k in single_counter])
-    df = pd.DataFrame(final_list_single, columns=['count','fracture_type'])
+    final_list_single = zip([float(v) for v in single_counter.values()], [
+                            k for k in single_counter])
+    df = pd.DataFrame(final_list_single, columns=['count', 'fracture_type'])
     df_draw = df.copy()
     df_draw.loc[df_draw['count'] < 50, 'fracture_type'] = 'other'
     df_draw = df_draw.groupby('fracture_type')['count'].sum().reset_index()
-    plt.pie(df_draw['count'], labels=df_draw['fracture_type'], autopct='%1.1f%%')
+    plt.pie(df_draw['count'], labels=df_draw['fracture_type'],
+            autopct='%1.1f%%')
     plt.savefig("data_balance_figures/fracture_type_single_split")
     plt.clf()
-    final_list_multi = zip([float(v) for v in multi_counter.values()],[k for k in multi_counter])
-    df = pd.DataFrame(final_list_multi, columns=['count','fracture_type'])
+    final_list_multi = zip([float(v) for v in multi_counter.values()], [
+                           k for k in multi_counter])
+    df = pd.DataFrame(final_list_multi, columns=['count', 'fracture_type'])
     df_draw = df.copy()
     df_draw.loc[df_draw['count'] < 75, 'fracture_type'] = 'other'
     df_draw = df_draw.groupby('fracture_type')['count'].sum().reset_index()
-    plt.pie(df_draw['count'], labels=df_draw['fracture_type'], autopct='%1.1f%%')
+    plt.pie(df_draw['count'], labels=df_draw['fracture_type'],
+            autopct='%1.1f%%')
     plt.savefig("data_balance_figures/fracture_type_multi_split")
     plt.clf()
-    plt.pie([len(single_fractures),len(multi_fractures), len(unbroken)], labels = ['single break','multi-break','unbroken'], autopct='%1.1f%%')
+    plt.pie([len(single_fractures), len(multi_fractures), len(unbroken)], labels=[
+            'single break', 'multi-break', 'unbroken'], autopct='%1.1f%%')
     plt.savefig("data_balance_figures/single_multi_unbroken_split")
     plt.clf()
-    plt.pie([list_23,list_22,list_77,list_72, other], labels = ['23 break','22 break','77 break', '72 break', 'other'], autopct='%1.1f%%')
+    plt.pie([list_23, list_22, list_77, list_72, other], labels=[
+            '23 break', '22 break', '77 break', '72 break', 'other'], autopct='%1.1f%%')
     plt.savefig("data_balance_figures/single_fracture_general_type_split")
     plt.clf()
-    plt.pie([len(single_fractures),len(unbroken)], labels = ['Single Fracture', 'Unbroken'], autopct ='%1.1f%%')
+    plt.pie([len(single_fractures), len(unbroken)], labels=[
+            'Single Fracture', 'Unbroken'], autopct='%1.1f%%')
     plt.savefig("data_balance_figures/single_to_unbroken_split")
     plt.clf()
+
 
 def show_fracture_totals():
     print("Single Fracture:\n")
     single_counter.most_common()
     for (value, count) in single_counter.most_common():
-        print(value,count)
+        print(value, count)
     print("Multi Fracture:\n")
     multi_counter.most_common()
     for (value, count) in multi_counter.most_common():
-        print(value,count)
+        print(value, count)
+
+
 def main():
     # path = {directory for the annotation files in json format}
     gather_lists(path)
     total_fracture_types_single()
     show_balances()
+
+
 if __name__ == "__main__":
     main()
