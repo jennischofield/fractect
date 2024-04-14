0cd0b644025b7be6dbc05b707aafa5f7de526c7e
Jenni Schofield <jennischofield02@icloud.com>
2024-04-14 19:13:05 +0100

    Finished front end and linking to back end - project as complete as can be

--- gradcam.py
+++ gradcam.py
@@ -1,4 +1,6 @@
 import torch
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
 import PIL
@@ -6,6 +8,10 @@ from PIL import Image
 from matplotlib import colormaps
 from torchvision.transforms.functional import to_pil_image
 import numpy as np
+from traintest import load_resnext_model
+from adjustimage import AdjustImage
+from torchvision.transforms import v2
+import os
 # Tutorial used for this class is: https://towardsdatascience.com/grad-cam-in-pytorch-use-of-forward-and-backward-hooks-7eba5e38d569
 
 # defines two global scope variables to store our gradients and activations
@@ -36,30 +42,32 @@ def remove_hooks(backward_hook_var, forward_hook_var):
     forward_hook_var.remove()
 
 
-def run(model, image):
+def run_gradcam(model, image):
     # Accesses the relevant layer for ResNext model - if using other models, this would need to be changed
     final_layer = model.module.layer4[2]
     backward_hook_var = final_layer.register_full_backward_hook(
         backward_hook, prepend=False)
     forward_hook_var = final_layer.register_forward_hook(
         forward_hook, prepend=False)
+    
     model(image.unsqueeze(0)).sum().backward()
     pooled_gradients = torch.mean(gradients[0], dim=[0, 2, 3])
-    # weight the channels by corresponding gradients
-    for i in range(activations.size()[1]):
-        activations[:, i, :, :] *= pooled_gradients[i]
+    for i in range(2):
+        # weight the channels by corresponding gradients
+        for i in range(activations.size()[1]):
+            activations[:, i, :, :] *= pooled_gradients[i]
 
-    # average the channels of the activations
-    heatmap = torch.mean(activations, dim=1).squeeze()
+        # average the channels of the activations
+        heatmap = torch.mean(activations, dim=1).squeeze()
 
-    # relu on top of the heatmap
-    heatmap = F.relu(heatmap)
+        # relu on top of the heatmap
+        heatmap = F.relu(heatmap)
 
-    # normalize the heatmap
-    heatmap /= torch.max(heatmap)
+        # normalize the heatmap
+        heatmap /= torch.max(heatmap)
 
-    # draw the heatmap
-    plt.matshow(heatmap.detach().cpu())
+        # draw the heatmap
+        #plt.matshow(heatmap.detach().cpu())
 
     #Heatmap generated. Now overlay
     fig, ax = plt.subplots()
@@ -86,6 +94,21 @@ def run(model, image):
     # but with alpha < 1 (this defines the transparency of the heatmap)
     ax.imshow(overlay, alpha=0.4, interpolation='nearest', extent=None)
 
-
+    file_path = os.path.dirname(os.path.abspath(__file__))
     # Show the plot
-    plt.savefig("grad_cam_result.jpg",bbox_inches='tight',pad_inches=0.0)
+    plt.savefig(file_path + "/static/results/modelgradcamoutput.jpg",bbox_inches='tight',pad_inches=0.0)
+    #plt.show()
+def main():
+    DEVICE = torch.device(
+    'cuda') if torch.cuda.is_available() else torch.device('cpu')
+    CLASSIFICATION_TRANSFORMS = v2.Compose([v2.Resize([256,256]), v2.PILToTensor()])
+    print(os.getcwd())
+    model = load_resnext_model(DEVICE,"models\\classification_model.pth" )
+    image = Image.open("test_images\\0093_0597190523_02_WRI-R2_M012.jpg")
+    image = CLASSIFICATION_TRANSFORMS(image) 
+    
+    image = image.type(torch.FloatTensor)
+    run_gradcam(model, image)
+
+if __name__ == "__main__":
+    main()
